ba3c313bfbac7ccaaad93b6e5ec023b9bc8ef28f
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2007-02-26 13:59:53 +0000

    なぜかコミット漏れ...orz
    git-svn-id: https://www.seasar.org/svn/s2dao/branches/s2dao-tiger-1.0.x@764 a0133a49-3802-0410-849b-833ed23e894f

---
+++ s2-dao-tiger/src/test/java/org/seasar/dao/annotation/tiger/impl/DaoAnnotationReaderImplTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.dao.annotation.tiger.impl;
+
+import org.seasar.dao.DaoMetaDataFactory;
+import org.seasar.dao.annotation.tiger.Arguments;
+import org.seasar.dao.annotation.tiger.NoPersistentProperty;
+import org.seasar.dao.annotation.tiger.PersistentProperty;
+import org.seasar.dao.annotation.tiger.Query;
+import org.seasar.dao.annotation.tiger.S2Dao;
+import org.seasar.dao.annotation.tiger.Sql;
+import org.seasar.dao.impl.AbstractDao;
+import org.seasar.framework.beans.BeanDesc;
+import org.seasar.framework.beans.factory.BeanDescFactory;
+
+/**
+ * @author azusa
+ * 
+ */
+public class DaoAnnotationReaderImplTest extends
+        AbstractDaoAnnotationReaderImplTest {
+
+    protected void setUp() throws Exception {
+        clazz = AbstractAaaDaoImpl.class;
+        BeanDesc daoDesc = BeanDescFactory.getBeanDesc(clazz);
+        annotationReader = new DaoAnnotationReaderImpl(daoDesc);
+        aaaClazz = Aaa.class;
+        daoClazz = AaaDao.class;
+    }
+
+    @S2Dao(bean = Aaa.class)
+    public static interface AaaDao {
+
+        @Arguments( { "aaa1", "aaa2" })
+        public Aaa getAaaById1(int id);
+
+        @Query("A > B")
+        public Aaa getAaaById2(int id);
+
+        @Sql("SELECT * FROM AAA")
+        public Aaa getAaaById3(int id);
+
+        @NoPersistentProperty("abc")
+        public Aaa createAaa1(Aaa aaa);
+
+        @PersistentProperty("def")
+        public Aaa createAaa2(Aaa aaa);
+
+    }
+
+    public static interface Aaa2Dao extends AaaDao {
+    }
+
+    public static abstract class AbstractAaaDaoImpl extends AbstractDao
+            implements Aaa2Dao {
+
+        public AbstractAaaDaoImpl(DaoMetaDataFactory daoMetaDataFactory) {
+            super(daoMetaDataFactory);
+        }
+
+    }
+
+    public static class Aaa {
+    }
+
+}
